0b7d0b056d87bbf3a3b1c8ed291c538febfedc66
Keenjin <523059842@qq.com>
2017-12-22 03:02:47 +0800

    query info

--- WinApp/MSAATest/MSAATest/PolicyObjImpl.h
+++ WinApp/MSAATest/MSAATest/PolicyObjImpl.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "PolicyObj.h"
+#include "AutoCriticalSection.h"
 
 class CPolicyObj
 	: public IUnknownImplT<IPolicyObj>
@@ -18,12 +19,14 @@ public:
 
 	virtual HRESULT STDMETHODCALLTYPE SetParam(UINT uIndex, VARIANT varVal)
 	{
+		CAutoCriticalSection lock(m_csForMap);
 		m_mapKeyValue.insert(std::pair<UINT, ATL::CComVariant>(uIndex, varVal));
 		return S_OK;
 	}
 
 	virtual HRESULT STDMETHODCALLTYPE GetParam(UINT uIndex, VARIANT* pvarVal)
 	{
+		CAutoCriticalSection lock(m_csForMap);
 		if (m_mapKeyValue.find(uIndex) != m_mapKeyValue.end())
 		{
 			*pvarVal = m_mapKeyValue[uIndex];
@@ -35,9 +38,14 @@ public:
 
 private:
 	CPolicyObj(){}
-	virtual ~CPolicyObj() {}
+	virtual ~CPolicyObj() 
+	{
+		CAutoCriticalSection lock(m_csForMap);
+		m_mapKeyValue.clear();
+	}
 
 private:
+	ATL::CComAutoCriticalSection m_csForMap;
 	std::map<UINT, ATL::CComVariant> m_mapKeyValue;
 };
 
